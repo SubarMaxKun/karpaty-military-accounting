6bbba7a1251b38ea24e6544375c554b4699cf1e0
SubarMaxKun <maxgamer0027@gmail.com>
2024-04-16 10:24:45 +0300

    Edited data validation for rank

--- src/main/java/org/shevliakov/carpatymilitaryaccounting/database/repository/RankRepository.java
+++ src/main/java/org/shevliakov/carpatymilitaryaccounting/database/repository/RankRepository.java
@@ -2,10 +2,13 @@ package org.shevliakov.carpatymilitaryaccounting.database.repository;
 
 import org.shevliakov.carpatymilitaryaccounting.entity.Rank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RankRepository extends JpaRepository<Rank, Long> {
 
   Rank findByName(String name);
+
+  boolean existsByName(String name);
 }
